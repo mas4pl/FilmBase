08cb1c576cf2e4e15e8fa6cc5c19dbd2910e639a
mas4pl <mas4pl.pl@gmail.com>
2020-04-07 12:21:23 +0200

    FilmBase

---
+++ addfilms/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AddfilmsConfig(AppConfig):
+    name = 'addfilms'
---
+++ addfilms/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def index(request):
+  return HttpResponse("kiedys to moze bedzie baza filmow :)")
