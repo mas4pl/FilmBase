c91a985f17797d6b84b4f7e89cb3709bc4a9e178
mas4pl <mas4pl.pl@gmail.com>
2020-04-21 11:09:37 +0200

    login + html

--- addfilms/views.py
+++ addfilms/views.py
@@ -1,4 +1,28 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from .forms import LoginFrom
 
-def index(request):
-  return HttpResponse("kiedys to moze bedzie baza filmow :)")
+def user_login(request):
+  if request.method == 'POST':
+    form = LoginForm(request.POST)
+    if form.is_valid():
+      cd = form.cleaned_data
+      user = authenticate(username=cd['username'],
+                          password=cd['password'])
+      if user is not None:
+        if user.is_active:
+          login(request, user)
+          return HttpResponse('Uwierzytelnienie zakonczone sukcesem.')
+        else:
+          return HttpResponse('Konto zablokowane.')
+      else:
+        return HttpResponse('Nieprawidlowe daneuwierzytelniajace')
+  else:
+    form = LoginForm()
+  return render(request, 'addfilms/login.html', {'form': form})
+          
+  
+
+#def index(request):
+  #return HttpResponse("kiedys to moze bedzie baza filmow :)")
