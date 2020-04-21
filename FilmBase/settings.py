48e458cd70efd526ae040e5571d2107a257ed056
Andrzej Szary <aszary@gmail.com>
2020-04-21 11:48:10 +0200

    Movie model added

--- FilmBase/settings.py
+++ FilmBase/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'addfilms.apps.AddfilmsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- addfilms/models.py
+++ addfilms/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    director = models.CharField(max_length=200)
+    year = models.DateField()
+    gener = models.CharField(max_length=100)
